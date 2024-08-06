12169220542e03b0b5d625c9c296f409f6891617
Eldotest9 <127268436+Eldotest9@users.noreply.github.com>
2024-08-06 20:05:01 +0900

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -4,7 +4,15 @@ st.balloons()
 st.title("Happy Birthday 🎈")
 
 
-VIDEO_URL = "https://www.youtube.com/watch?v=NT3zp2vYMvo"
-st.video(VIDEO_URL,start_time=2)
+slider_value = st.slider('Select a value', min_value=0, max_value=100, value=50)
+if (slider_value <51):
+    VIDEO_URL = "https://www.youtube.com/watch?v=NT3zp2vYMvo"
+else:
+    VIDEO_URL = "https://www.youtube.com/watch?v=Ctf2g_-VZbo&ab_channel=ShyamPankhania"
+
+
+st.video(VIDEO_URL,start_time=0)
+
+
 
 
